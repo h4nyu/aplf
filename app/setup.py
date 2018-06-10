0dd3736f546b2d173c95a7f2a3bed2f64a5625c1
x1nyuan <yao.ntno@gmail.com>
2018-06-10 16:35:22 +0900

    fix pip 10 compat

--- app/setup.py
+++ app/setup.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pip
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+try:  # for pip >= 10
+    from pip._internal.download import PipSession
+except ImportError:  # for pip <= 9.0.3
+    from pip.download import PipSession
 from setuptools import setup, find_packages
 
 install_reqs = parse_requirements(
     'requirements.txt',
-    session=pip.download.PipSession,
+    session=PipSession,
 )
 
 install_reqs = [str(ir.req) for ir in install_reqs]
 
 dev_reqs = parse_requirements(
     'dev-requirements.txt',
-    session=pip.download.PipSession
+    session=PipSession
 )
 
 dev_reqs = [str(ir.req) for ir in dev_reqs]
