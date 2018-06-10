368d98cd394c3dda3d5b4f9d6791f29bf9184248
x1nyuan <yao.ntno@gmail.com>
2018-06-10 18:41:58 +0900

    build passed

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,27 +1,48 @@
 const path = require('path');
-const setPath = function(folderName) {
-  return path.join(__dirname, folderName);
-}
+const { VueLoaderPlugin } = require("vue-loader");
+
+
 const NODE_ENV = process.env.NODE_ENV;
 const config = {
   mode: NODE_ENV,
-  entry: './src/index.js',
+  entry: './src/main.js',
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.css$/,
         use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['env', {'modules': false}]
-              ]
-            }
-          }
-        ]
-      }
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // Babel のオプションを指定する
+        options: {
+          presets: [
+            // env を指定することで、ES2017 を ES5 に変換。
+            // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
+            // webpack の Tree Shaking 機能が使えない
+            ['env', {'modules': false}]
+          ]
+        },
+        exclude: /node_modules/
+      },
     ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
   }
 };
 module.exports = config;
