9e211a79c3fbe2012fad608018a5b003abc6fd37
x1nyuan <yao.ntno@gmail.com>
2018-06-13 23:44:32 +0900

    add bokeh

--- app/dist.py
+++ app/dist.py
@@ -1,12 +1,29 @@
 
-from dask.distributed import Client
-client = Client('dask_scheduler:8786')  # set up local cluster on your laptop
-print(client)
+from distributed import Client
+from time import sleep
+import random
 
 
 def inc(x):
+    sleep(random.random() / 10)
     return x + 1
 
 
-x = client.submit(inc, 10)
-L = client.map(inc, range(1000))
+def dec(x):
+    sleep(random.random() / 10)
+    return x - 1
+
+
+def add(x, y):
+    sleep(random.random() / 10)
+    return x + y
+
+
+client = Client('dask_scheduler:8786')  # set up local cluster on your laptop
+incs = client.map(inc, range(10000))
+decs = client.map(dec, range(10000))
+adds = client.map(add, incs, decs)
+total = client.submit(sum, adds)
+
+del incs, decs, adds
+print(total.result())
