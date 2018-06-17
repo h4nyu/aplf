87689f25a5c1bd395e9dfce1d936fc94c1f0f232
x1nyuan <yao.ntno@gmail.com>
2018-06-17 23:48:23 +0900

    update util

--- app/graph.py
+++ app/graph.py
@@ -1,23 +1,8 @@
-from distributed import Client, progress
+from distributed import Client
 from joblib import Memory
-from dask import visualize
-from time import sleep
-from cytoolz.curried import pipe, map, take
-import torch.nn.functional as F
-import dask.bag as db
-import numpy as np
-import random
-import dask.array as da
-import torch
-import torch.optim as optim
-from pprint import pprint
-from aplf.utils import merge_kwargs
-from dateutil.parser import parse
-from dateutil.relativedelta import relativedelta
-from torch.utils.data import DataLoader
-from aplf.dataset import DummyDataset
-from aplf.model import Autoencoder
 from aplf.plot import plot
+from aplf.utils import merge_kwargs
+import numpy as np
 
 
 memory = Memory(cachedir='/', verbose=0)
@@ -29,7 +14,7 @@ def gen_dummy():
 
 dsk = {
     'dummy_data': (gen_dummy,),
-    "plot": (merge_kwargs({'loss': 'z', 'batch_idx': 'x'})(plot), "dummy_data", {
+    "plot": (merge_kwargs({'loss': 'y', 'batch_idx': 'x'})(plot), "dummy_data", {
         'from_idx': 0,
         "to_idx": 10000,
         'path': '/data/plot.png'
