448dc8624f3c091c98b4cb5c4e082f29453d1bdc
x1nyuan <yao.ntno@gmail.com>
2018-07-01 20:02:54 +0900

    train

--- app/kaggle/titanic/graph.py
+++ app/kaggle/titanic/graph.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from distributed import Client
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from dask import delayed
+import torch
 import pandas as pd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 
 @delayed
@@ -31,9 +35,56 @@ class TitanicDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
-        data = self.df.iloc[idx][['Age', 'SibSp', 'Fare', 'Pclass']].values
-        label = self.df.iloc[idx]['Survived']
-        return data, label
+        data = self.df.iloc[idx][['Age', 'SibSp',
+                                  'Fare', 'Pclass']].values.astype(float)
+        label = self.df.iloc[idx][['Survived']]
+        return torch.FloatTensor(data), torch.FloatTensor([float(label)])
+
+
+class TitanicNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc0 = nn.Linear(4, 4)
+        self.fc1 = nn.Linear(4, 4)
+        self.fc2 = nn.Linear(4, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc0(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return x
+
+
+@delayed
+def plot(x,
+         path,
+         **kwargs):
+    import matplotlib as mpl
+    mpl.use('Agg')
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(x)
+    plt.savefig(path)
+    plt.close()
+    return path
+
+
+@delayed
+def train(dataset):
+    loader = DataLoader(dataset)
+    device = torch.device("cpu")
+    model = TitanicNet().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    losses = []
+    for batch_idx, (data, label) in enumerate(loader):
+        data, label = data.to(device), label.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.mse_loss(output, label)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+    return losses
 
 
 if __name__ == '__main__':
@@ -41,9 +92,11 @@ if __name__ == '__main__':
     train_df = delayed(pd.read_csv)('/data/titanic/train.csv')
     preprocessed = preprocess(train_df)
     train_dataset = delayed(TitanicDataset)(preprocessed)
+    trained_loss = train(train_dataset)
+    loss_plot = plot(trained_loss, '/data/loss.png')
 
     with Client('dask-scheduler:8786') as c:
-        train_dataset.visualize('/data/graph.svg')
+        loss_plot.visualize('/data/graph.svg')
 
-        result = c.compute(train_dataset, sync=True)
-        print(result[0])
+        result = c.compute(loss_plot, sync=True)
+        print(result)
