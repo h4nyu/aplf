a48c3d489eb962fdf4cc2f4310d012d3119611a9
x1nyuan <yao.ntno@gmail.com>
2018-08-16 10:56:02 +0900

    add test train

---
+++ app/tests/kaggle/tgs/test_train.py
@@ -0,0 +1,17 @@
+from aplf.kaggle.tgs.train import train
+from aplf.kaggle.tgs.dataset import TgsSaltDataset, load_dataset_df
+from sklearn.model_selection import train_test_split
+
+
+def test_train():
+    dataset_df = load_dataset_df('/store/kaggle/tgs')
+    train_df, val_df = train_test_split(dataset_df)
+    output_dir = '/store/tmp'
+    train(
+        model_path=f"{output_dir}/model.pt",
+        train_dataset=TgsSaltDataset(train_df),
+        val_dataset=TgsSaltDataset(val_df),
+        epochs=1,
+        batch_size=128,
+        patience=10,
+    )
