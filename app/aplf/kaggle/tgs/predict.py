cd55cc3b92648d05d82856d088a0df1e203032f0
x1nyuan <yao.ntno@gmail.com>
2018-09-19 14:05:57 +0900

    add nograd

--- app/aplf/kaggle/tgs/predict.py
+++ app/aplf/kaggle/tgs/predict.py
@@ -38,42 +38,44 @@ def predict(model_paths,
     scores = []
 
     n_itr = 0
-    for sample in loader:
-        sample_id = sample['id'][0]
-        image = sample['image'].to(device)
 
-        output = pipe(
-            models,
-            map(lambda x: x(image)),
-            map(lambda x: x.softmax(dim=1)),
-            reduce(lambda x, y: x + y / 2),
-            lambda x: F.softmax(x, dim=1),
-            lambda x: x.argmax(dim=1).float()
-        )
-        sample_ids.append(sample_id)
-        rle_masks.append(rl_enc(output.cpu().numpy().reshape(101, 101)))
+    with torch.no_grad():
+        for sample in loader:
+            sample_id = sample['id'][0]
+            image = sample['image'].to(device)
 
-        log_images = [image[0], output]
-        if 'mask' in sample.keys():
-            mask = sample['mask'].to(device)[0]
-            log_images.append(mask)
-            score = iou(output.cpu().numpy(), mask.cpu().numpy())
-            scores.append(score)
+            output = pipe(
+                models,
+                map(lambda x: x(image)),
+                map(lambda x: x.softmax(dim=1)),
+                reduce(lambda x, y: x + y / 2),
+                lambda x: F.softmax(x, dim=1),
+                lambda x: x.argmax(dim=1).float()
+            )
+            sample_ids.append(sample_id)
+            rle_masks.append(rl_enc(output.cpu().numpy().reshape(101, 101)))
 
-        if n_itr % log_interval == 0:
-            with SummaryWriter(log_dir) as w:
-                w.add_image(
-                    f"predict",
-                    vutils.make_grid(log_images, scale_each=True),
-                    n_itr
-                )
+            log_images = [image[0], output]
+            if 'mask' in sample.keys():
+                mask = sample['mask'].to(device)[0]
+                log_images.append(mask)
+                score = iou(output.cpu().numpy(), mask.cpu().numpy())
+                scores.append(score)
 
-        n_itr += 1
+            if n_itr % log_interval == 0:
+                with SummaryWriter(log_dir) as w:
+                    w.add_image(
+                        f"predict",
+                        vutils.make_grid(log_images, scale_each=True),
+                        n_itr
+                    )
 
-    df['id'] = sample_ids
-    df['rle_mask'] = rle_masks
-    if len(scores) > 0:
-        df['score'] = scores
-    df = df.set_index('id')
+            n_itr += 1
 
-    return df
+        df['id'] = sample_ids
+        df['rle_mask'] = rle_masks
+        if len(scores) > 0:
+            df['score'] = scores
+        df = df.set_index('id')
+
+        return df
