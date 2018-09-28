fce700dc9297e6452a8e0190c6ff9be3db93134f
x1nyuan <yao.ntno@gmail.com>
2018-09-28 11:15:06 +0900

    eunet

--- app/tests/kaggle/tgs/test_graph.py
+++ app/tests/kaggle/tgs/test_graph.py
@@ -6,22 +6,22 @@ from datetime import datetime
 
 def test_graph():
     g = Graph(
-        id="24-4-eunet-ema-0.2-consistency-0.2-bs-24",
+        id="8-4-eunet-ema-0.2-consistency-0.2-bs-24-rampup-100",
         dataset_dir='/store/kaggle/tgs',
         output_dir='/store/kaggle/tgs/output',
         epochs=400,
-        labeled_batch_size=24,
-        no_labeled_batch_size=8,
-        model_type='UNet',
+        labeled_batch_size=32,
+        no_labeled_batch_size=4,
+        model_type='EUNet',
         val_split_size=0.2,
-        feature_size=24,
+        feature_size=8,
         depth=4,
         patience=20,
         reduce_lr_patience=6,
         base_size=10,
         ema_decay=0.2,
         consistency=0.2,
-        consistency_rampup=20,
+        consistency_rampup=100,
         parallel=1,
         top_num=1,
     )
