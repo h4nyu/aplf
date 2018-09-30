76ae15d9ce7640c7d8052d4761d09aa6fff4cb17
Xinyuan Yao <yao.ntno@gmail.com>
2018-09-30 12:17:40 +0000

    revert dataset

--- app/aplf/kaggle/tgs/dataset.py
+++ app/aplf/kaggle/tgs/dataset.py
@@ -1,4 +1,16 @@
 from cytoolz.curried import keymap, filter, pipe, merge, map, compose
+from torchvision.transforms import (
+    RandomRotation,
+    ToPILImage,
+    Compose, ToTensor,
+    CenterCrop,
+    RandomAffine,
+    TenCrop,
+    RandomApply,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    RandomResizedCrop,
+)
 import random
 from skimage import io
 import torch
@@ -9,7 +21,7 @@ import os
 import glob
 import random
 import torch.nn.functional as F
-from .preprocess import hflip, vflip, crop
+from torchvision.transforms.functional import hflip, vflip, rotate
 
 
 def load_dataset_df(dataset_dir, csv_fn='train.csv'):
@@ -38,6 +50,11 @@ class TgsSaltDataset(Dataset):
     def __init__(self, df, has_y=True):
         self.has_y = has_y
         self.df = df
+        self.transforms = [
+            lambda x:x,
+            hflip,
+        ]
+
 
     def __len__(self):
         return len(self.df)
@@ -45,54 +62,35 @@ class TgsSaltDataset(Dataset):
     def __getitem__(self, idx):
         id = self.df.index[idx]
         depth = self.df['z'].iloc[idx],
-        img_ary = io.imread(
-            self.df['x_image'].iloc[idx],
-            as_gray=True
-        )
-        h, w = img_ary.shape
-        limit_h = h // 5
-        limit_w = w // 5
-        start = (
-            np.random.randint(0, limit_w),
-            np.random.randint(0, limit_h),
-        )
-
-        end = (
-            np.random.randint(w - limit_w, w),
-            np.random.randint(h - limit_h, h),
-        )
-
-        transforms = [
-            torch.FloatTensor,
-            lambda x: x.view(1, h, w),
-        ]
-
-        if self.has_y:
-            mask_ary = io.imread(
-                self.df['y_mask_true'].iloc[idx],
+        image = torch.FloatTensor(
+            io.imread(
+                self.df['x_image'].iloc[idx],
                 as_gray=True
-            ).astype(bool).astype(int)
-
-            transform = compose(*reversed([
-                *transforms,
-                random.choice([
-                    lambda x: x,
-                    hflip,
-                ]),
-            ]))
-
+            ).reshape(1, 101, 101)
+        )
+        if self.has_y:
+            transform = Compose([
+                ToPILImage(),
+                random.choice(self.transforms),
+                random.choice(self.transforms),
+                ToTensor()
+            ])
+            mask = torch.FloatTensor(
+                io.imread(
+                    self.df['y_mask_true'].iloc[idx],
+                    as_gray=True
+                ).astype(bool).astype(float).reshape(1, 101, 101)
+            )
             return {
                 'id': id,
                 'depth': depth,
-                'image': transform(img_ary),
-                'mask': transform(mask_ary),
+                'image': transform(image),
+                'mask': transform(mask),
             }
         else:
-            transform = compose(*reversed([
-                *transforms
-            ]))
             return {
                 'id': id,
                 'depth': depth,
-                'image': transform(img_ary),
+                'image': image,
             }
+
