c942a80e15a0935fd1554d42e498e045828b2cd5
Xinyuan Yao <yao.ntno@gmail.com>
2018-10-10 11:16:29 +0000

    test model

--- app/tests/kaggle/tgs/test_graph.py
+++ app/tests/kaggle/tgs/test_graph.py
@@ -14,10 +14,10 @@ def test_graph():
         'epochs': 400,
         'batch_size': 32,
         'model_type': 'HUNet',
-        'erase_num': 10,
+        'erase_num': 5,
         'erase_p': 1,
         'model_kwargs': {
-            'feature_size': 32,
+            'feature_size': 56,
         },
 
     }
@@ -37,7 +37,7 @@ def test_graph():
     }
     g = Graph(
         **base_param,
-        id="19",
+        id="20",
         base_train_config=base_train_config,
         fine_train_config=fine_train_config,
         n_splits=8,
