ec017f0c380539c298e8e78dcdbfb115893486c0
xinyuan yao <yao.ntno@gmail.com>
2018-11-27 21:35:55 +0000

    add metrics

--- app/aplf/tellus/train/multi.py
+++ app/aplf/tellus/train/multi.py
@@ -299,6 +299,7 @@ def train_multi(model_path,
                 )
                 dump_json(f'{model_path}.json', {
                     **val_metrics,
+                    'threshold': train['threshold'],
                     "create_date": datetime.now().isoformat(),
                 })
                 torch.save(model, model_path)
