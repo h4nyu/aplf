099d3ddb38e183a91788c0cd02e8aeacf591800b
xinyuan yao <yao.ntno@gmail.com>
2018-11-28 15:11:27 +0000

    store/tellus/output/bs-32-split-8-rerase-10-landsatw-1-shuffle-pixel-dropout-0.1-cel-fusion-th-dyn-small-fusion-pad-4-rs-64-landsat-scse-in-res-lr-0.001-esb-1-fs-16-dp-3-4-single-optim-elu-elu/fold-0

--- app/tests/tellus/test_graph.py
+++ app/tests/tellus/test_graph.py
@@ -11,7 +11,7 @@ def test_graph():
     config = {
         "train_config": {
             'epochs': 1000,
-            'batch_size': 128,
+            'batch_size': 32,
             'model_kwargs': {
                 'feature_size': 16,
                 'resize': 64,
@@ -22,7 +22,7 @@ def test_graph():
             'landsat_weight': 1,
         },
         "dataset_dir": '/store/tellus',
-        "output_dir": '/store/tellus/output/split-8-rerase-10-landsatw-1-shuffle-pixel-dropout-0.1-cel-fusion-th-dyn-small-fusion-pad-4-rs-64-landsat-scse-in-res-lr-0.001-esb-1-fs-16-dp-3-4-single-optim-elu-elu',
+        "output_dir": '/store/tellus/output/bs-32-split-8-rerase-10-landsatw-1-shuffle-pixel-dropout-0.1-cel-fusion-th-dyn-small-fusion-pad-4-rs-64-landsat-scse-in-res-lr-0.001-esb-1-fs-16-dp-3-4-single-optim-elu-elu',
         'n_splits': 8,
         'folds': [0],
         'seed': 0
