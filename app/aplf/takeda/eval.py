ce5fbd8ace289a1644477b34138f822b55ae7984
h4nyu <yao.ntno@gmail.com>
2019-08-31 16:04:01 +0000

    remove mypy_cache

--- app/aplf/takeda/eval.py
+++ app/aplf/takeda/eval.py
@@ -1,2 +1,8 @@
-def r2() -> float:
-    return 0
+import typing as t
+
+
+def r2(
+    gt: t.Any,
+    pred: t.Any,
+) -> float:
+    return 1 - ((pred - gt)**2).sum()/((pred - gt.mean())**2).sum()
--- app/tests/takeda/test_eval.py
+++ app/tests/takeda/test_eval.py
@@ -1,5 +1,8 @@
 from aplf.takeda.eval import r2
+import numpy as np
 
 
 def test_r2() -> None:
-    r2()
+    x_arr = np.arange(10)
+    y_arr = np.arange(10)
+    assert r2(x_arr, y_arr) == 1.0
