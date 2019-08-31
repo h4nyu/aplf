0063f0d7b6c8c5453863ba6d0a3008be04ee60f4
h4nyu <yao.ntno@gmail.com>
2019-08-31 14:36:06 +0000

    pip -> pipenv

--- app/setup.py
+++ app/setup.py
@@ -1,45 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-try:  # for pip >= 10
-    from pip._internal.download import PipSession
-except ImportError:  # for pip <= 9.0.3
-    from pip.download import PipSession
 from setuptools import setup, find_packages
 
-install_reqs = parse_requirements(
-    'requirements.txt',
-    session=PipSession,
-)
-
-install_reqs = [str(ir.req) for ir in install_reqs]
-
-dev_reqs = parse_requirements(
-    'dev-requirements.txt',
-    session=PipSession
-)
-
-dev_reqs = [str(ir.req) for ir in dev_reqs]
-
+requires = [
+    "cytoolz",
+    "torchvision",
+    "torch",
+]
+dev_requires = [
+    "requests",
+    "pytest",
+    "pytest-mock",
+    "pytest-cov",
+    "faker",
+    "flake8",
+    "autopep8",
+    "pytest-asyncio",
+    "profilehooks",
+    "mypy",
+    "pytest-mypy",
+    "typing_extensions",
+]
 
 setup(
     name="aplf",
-    version="0.1",
+    version="0.0.0",
     description="TODO",
     author='Xinyuan Yao',
-    author_email='yao.ntno@gmail.co.jp',
+    author_email='yao.ntno@google.com',
     license="TODO",
     packages=find_packages(),
-    setup_requires=[
-        "pip-tools"
-    ],
-    install_requires=install_reqs,
+    install_requires=requires,
     extras_require={
-        'dev': dev_reqs,
+        'dev': dev_requires
     }
 )
+
