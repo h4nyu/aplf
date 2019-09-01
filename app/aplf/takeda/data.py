9680682191739c15d7b4fbc219a22dcd7d94260f
h4nyu <yao.ntno@gmail.com>
2019-09-01 04:39:08 +0000

    add dataset

--- app/aplf/takeda/data.py
+++ app/aplf/takeda/data.py
@@ -11,11 +11,13 @@ def read_csv(path: str) -> t.Any:
 
 class TakedaDataset:
     def __init__(self, df:t.Any) -> None:
-        self.df = df
+        self.series_y = df['Score']
+        self.df_x = df.drop('Score', axis=1)
 
     def __len__(self) -> int:
-        return len(self.df)
+        return len(self.series_y)
 
-    def __getitem__(self, idx:int) -> Tensor:
-        row = self.df.loc[idx]
-        return tensor(row)
+    def __getitem__(self, idx:int) -> t.Tuple[Tensor, Tensor]:
+        x = self.df_x.iloc[idx]
+        y = self.series_y[idx]
+        return tensor(x), tensor(y)
--- app/tests/takeda/test_data.py
+++ app/tests/takeda/test_data.py
@@ -1,6 +1,8 @@
 from aplf.takeda.data import read_csv, TakedaDataset
 from torch.utils.data import Dataset
 import pandas as pd
+from torch import tensor
+import torchvision
 
 def test_read_csv() -> None:
     df = read_csv('/store/takeda/train.csv')
@@ -8,7 +10,12 @@ def test_read_csv() -> None:
 
 def test_dataset() -> None:
     df = pd.DataFrame({
-        'Score': [0, 0],
-        'col1': [0, 0],
+        'Score': [0., 1.],
+        'col1': [0., 1.],
+        'col2': [0., 1.],
     }, index=[0, 1])
     dataset = TakedaDataset(df)
+    assert  all(tensor([0., 0.]) == dataset[0][0])
+    assert  tensor(0.) == dataset[0][1]
+    assert  all(tensor([1., 1.]) == dataset[1][0])
+    assert  tensor(1.) == dataset[1][1]
