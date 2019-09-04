cf7858d9fb13662f33607ae3f007718010d477ff
h4nyu <yao.ntno@gmail.com>
2019-09-04 14:59:23 +0000

    start index 1

--- app/takeda.py
+++ app/takeda.py
@@ -2,8 +2,8 @@ from aplf.takeda.app import run, submit
 
 
 if __name__ == '__main__':
-    #  run(
-    #      n_splits=10,
-    #      fold_idx=0,
-    #  )
-    submit('/store/model-0')
+    run(
+        n_splits=10,
+        fold_idx=1,
+    )
+    #  submit('/store/model-1')
