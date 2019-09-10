1781168813e5b62c8b9929ab32e2feb779bb9e00
xinyuan yao <yao.ntno@gmail.com>
2019-09-10 11:59:10 +0000

    statble optm

--- app/tests/takeda/test_app.py
+++ app/tests/takeda/test_app.py
@@ -1,18 +1,19 @@
 from aplf.takeda.app import run, submit, pre_submit, explore
 import pytest
 
+BASE_DIR="/store/bottleneck"
 def test_run_0() -> None:
     """
     submit 0.615
     local 0.448
     """
-    run(base_dir="/store/bottleneck", n_splits=10, fold_idx=0)
+    run(base_dir=BASE_DIR, n_splits=10, fold_idx=1)
 
 def test_submit() -> None:
-    submit(base_dir="/store/aug")
+    submit(base_dir=BASE_DIR)
 
 def test_pre_submit() -> None:
-    pre_submit(base_dir="/store/bottleneck")
+    pre_submit(base_dir=BASE_DIR)
 
 @pytest.mark.asyncio
 async def test_explore() -> None:
